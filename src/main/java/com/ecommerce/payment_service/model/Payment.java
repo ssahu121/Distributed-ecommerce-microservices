b9383d1aa17faf178628688b594e2e0a13c3d7f0
joyslin-cloud <joyslin111@gmail.com>
2026-05-21 22:27:19 +0530

    Added payment service

---
+++ src/main/java/com/ecommerce/payment_service/model/Payment.java
@@ -0,0 +1,27 @@
+package com.ecommerce.payment_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderId;
+
+    private Double amount;
+
+    private String paymentMethod;
+
+    private String paymentStatus;
+
+}
