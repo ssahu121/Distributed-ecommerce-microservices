28a8ae1af4351b324c1f71348b26932232f4e94e
Abhishek Bhosale <abhishekbhosale123@gmail.com>
2026-06-17 14:32:09 +0530

    Enhanced Payment Service with CRUD operations and status update APIs

--- payment-service/src/main/java/com/ecommerce/payment_service/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/ecommerce/payment_service/repository/PaymentRepository.java
@@ -1,7 +1,13 @@
+
 package com.ecommerce.payment_service.repository;
 
 import com.ecommerce.payment_service.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByOrderId(Long orderId);
+
 }
