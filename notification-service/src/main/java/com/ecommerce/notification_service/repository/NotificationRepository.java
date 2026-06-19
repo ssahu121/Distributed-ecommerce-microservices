9176f6b8f011f2248a8e239dff23eac62dabc70d
ssahuji910@gmail.com <ssahuji910@gmail.com>
2026-06-19 11:18:43 +0530

    updated

--- notification-service/src/main/java/com/ecommerce/notification_service/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.notification_service.repository;
-
-import com.ecommerce.notification_service.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
